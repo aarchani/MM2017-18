da1ebd41b7e42ca6ad564315f648053a964d6a03
Sushanth Mukkamalla <sushanthmukkamalla@gmail.com>
2018-05-27 03:26:16 -0700

    Update irled.c

--- Src/irled.c
+++ Src/irled.c
@@ -4,10 +4,28 @@
 double IR_MULTIPLIER = 1.0;
 
 void IR_Init() {
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_SET);
+  //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_SET);
 }
 
 // whichLed is 0-3
 double IR_GetDistance(uint8_t whichLed) {
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+
+	if(whichLed == IR_LED_RIGHT) {
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+	}
+	else if (whichLed == IR_LED_FRONT_RIGHT) {
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
+	}
+	else if (whichLed == IR_LED_FRONT_LEFT) {
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
+	}
+	else if (whichLed == IR_LED_LEFT) {
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET);
+	}
+	else {
+		return -1 * IR_MULTIPLIER;
+	}
+
 	return ADC_readValue(whichLed) * IR_MULTIPLIER;
 }
